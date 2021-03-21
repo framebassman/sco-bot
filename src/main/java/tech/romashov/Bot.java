0bf45d4aec4190b042ac13194cbe3f4154c25f55
Dmitry Romashov <framebassman@gmail.com>
2021-03-22 00:28:30 +0300

    Feature/skeleton (#1)
    * Add logs
    
    * Add skeleton from article
    
    * add skeleton
    
    * fix CI?

---
+++ src/main/java/tech/romashov/Bot.java
@@ -0,0 +1,53 @@
+package tech.romashov;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+// Аннотация @Component необходима, чтобы наш класс распознавался Spring, как полноправный Bean
+@Component
+// Наследуемся от TelegramLongPollingBot - абстрактного класса Telegram API
+public class Bot extends TelegramLongPollingBot {
+    // Аннотация @Value позволяет задавать значение полю путем считывания из application.yml
+    @Value("${bot.name}")
+    private String botUsername;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    private Logger log;
+
+    public Bot() {
+        log = LoggerFactory.getLogger(Application.class);
+    }
+
+    /* Перегружаем метод интерфейса LongPollingBot
+    Теперь при получении сообщения наш бот будет отвечать сообщением Hi!
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            log.info("Got message. Try to reply");
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(update.getMessage().getChatId()));
+            message.setText("Hi!");
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    // Геттеры, которые необходимы для наследования от TelegramLongPollingBot
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    public String getBotToken() {
+        return botToken;
+    }
+}
