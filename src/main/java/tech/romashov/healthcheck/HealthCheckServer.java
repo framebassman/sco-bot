e943ed563d59c78796b7f147f6f23173c7e0292f
Dmitry Romashov <framebassman@gmail.com>
2021-03-22 20:03:27 +0300

    fix it

--- src/main/java/tech/romashov/healthcheck/HealthCheckServer.java
+++ src/main/java/tech/romashov/healthcheck/HealthCheckServer.java
@@ -4,55 +4,22 @@ import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import tech.romashov.Application;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 @Component
 public class HealthCheckServer {
-    private Logger log;
-
     public HealthCheckServer() throws Exception {
-        log = LoggerFactory.getLogger(Application.class);
-        logEnvVariables();
-
         Server server = new Server();
         ServerConnector connector = new ServerConnector(server);
-        connector.setPort(8080);
+        if (System.getenv("PORT").equals("") || System.getenv("PORT").equals(null)) {
+            connector.setPort(8081);
+        } else {
+            connector.setPort(Integer.valueOf(System.getenv("PORT")));
+        }
         server.setConnectors(new Connector[] {connector});
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(HealthCheckServlet.class, "/");
         server.setHandler(context);
         server.start();
-    }
-
-    private void logEnvVariables() throws Exception {
-        Runtime rt = Runtime.getRuntime();
-        String[] commands = {"printenv"};
-        Process proc = rt.exec(commands);
-
-        BufferedReader stdInput = new BufferedReader(new
-                InputStreamReader(proc.getInputStream()));
-
-        BufferedReader stdError = new BufferedReader(new
-                InputStreamReader(proc.getErrorStream()));
-
-        // Read the output from the command
-        log.info("Here is the standard output of the command:\n");
-        String s = null;
-        while ((s = stdInput.readLine()) != null) {
-            System.out.println(s);
-        }
-
-        // Read any errors from the attempted command
-        log.info("Here is the standard error of the command (if any):\n");
-        while ((s = stdError.readLine()) != null) {
-            System.out.println(s);
-        }
     }
 }
