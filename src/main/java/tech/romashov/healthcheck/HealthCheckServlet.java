b0c443fa0c80934472a0f03b4b419eccc30bfe73
Dmitry Romashov <framebassman@gmail.com>
2021-03-22 17:00:58 +0300

    add healthcheck (#6)

---
+++ src/main/java/tech/romashov/healthcheck/HealthCheckServlet.java
@@ -0,0 +1,19 @@
+package tech.romashov.healthcheck;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HealthCheckServlet extends HttpServlet {
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("{ \"status\": \"ok\"}");
+    }
+}
