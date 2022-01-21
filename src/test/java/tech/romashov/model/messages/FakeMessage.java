1ca65dc1f03f90b24d42521090d6188b04efdeab
Dmitry Romashov <framebassman@gmail.com>
2022-01-22 00:22:28 +0300

    Tests/response (#15)
    * Add dummy test
    
    * Fix test

---
+++ src/test/java/tech/romashov/model/messages/FakeMessage.java
@@ -0,0 +1,13 @@
+package tech.romashov.model.messages;
+
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+public class FakeMessage extends Message {
+    public FakeMessage(String text) {
+        Chat chat = new Chat();
+        chat.setId(123L);
+        this.setChat(chat);
+        this.setText(text);
+    }
+}
