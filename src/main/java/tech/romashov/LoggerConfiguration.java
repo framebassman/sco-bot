96b2b18b0b0c30c91d8c57666cdc0f1e67054d4f
Dmitry Romashov <framebassman@gmail.com>
2022-01-22 00:34:20 +0300

    Logger throw DI (#16)

---
+++ src/main/java/tech/romashov/LoggerConfiguration.java
@@ -0,0 +1,19 @@
+package tech.romashov;
+
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Configuration
+public class LoggerConfiguration {
+    @Bean
+    @Scope("prototype")
+    public Logger produceLogger(InjectionPoint injectionPoint) {
+        Class<?> classOnWired = injectionPoint.getMember().getDeclaringClass();
+        return LoggerFactory.getLogger(classOnWired);
+    }
+}
